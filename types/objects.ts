9b8504c1d7eb6fc934387e684dff3da86726cb1e
sivashankar97 <hexshankar1997@gmail.com>
2022-11-23 11:55:17 +0530

    excess of properties

--- types/objects.ts
+++ types/objects.ts
@@ -3,3 +3,14 @@ function printName (person : { first :string}) {
 }
 
 printName ({first : "thomast"});
+
+
+
+
+let coordinate  : { x :number , y : number} = {x :34 , y :2};
+
+function makerandom() : {
+    x : number , y : number
+} {
+    return { x : Math.random(), y : Math.random()}
+}
