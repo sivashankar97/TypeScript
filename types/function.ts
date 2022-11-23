af3197e6ce2170de49be3a334618083d1ca02041
sivashankar97 <hexshankar1997@gmail.com>
2022-11-23 11:40:35 +0530

    retrun type annotation

--- types/function.ts
+++ types/function.ts
@@ -11,3 +11,12 @@ const dosomething = (person : string, age :number ) => {
 }
 
 dosomething("name ", 5);
+
+
+//function retrun type
+
+function squares ( num: number) : number{
+   return num * num ;
+}
+
+
