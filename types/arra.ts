99322de60e822c292e764f8a1b5f84072fccf44d
sivashankar97 <hexshankar1997@gmail.com>
2022-11-23 12:14:40 +0530

    multidimensational array

--- types/arra.ts
+++ types/arra.ts
@@ -1,3 +1,15 @@
 const activeuser : string [] = []
 
 activeuser.push('steven'); 
+
+
+const active : Array<string> [] = []
+
+type Point = {
+    x :number ;
+    y : number;
+}
+
+const coord : Point[] = []
+
+coord.push({x : 23 , y : 8}) ;
