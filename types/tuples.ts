fd69e19887e1fdcaa57f48c079a9ba8e9146fc81
sivashankar97 <hexshankar1997@gmail.com>
2022-11-30 13:13:39 +0530

    Bit more on tuples

--- types/tuples.ts
+++ types/tuples.ts
@@ -1,3 +1,10 @@
 let myTuple : [ number , string] ;
 
 myTuple = [ 10 , 'typescript fun']
+
+type HTTPresponse = [ number , string];
+
+const goodRes : HTTPresponse = [ 200 , " ok"]
+
+//goofRes[0] = "200"
+//goodRes.push(123)
